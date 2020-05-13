44a26bf3e3523a133150aaea7fc27875e10566f9
mhukill <65314778+mhukill@users.noreply.github.com>
2020-05-13 15:01:22 -0700

    Sequence Writer
    This is the reference genome we're using, and a first attempt at our R script that will be creating the FASTA files for BEAUTi.

---
+++ sequence.writer.r
@@ -0,0 +1,35 @@
+# Read in the data and references ##
+library("seqinr")
+reference.genome <- read.fasta("LR605956.fasta", as.string=TRUE, seqonly=TRUE)
+ref <- read.table("ref.tab", header=TRUE, sep="\t")
+non <- read.table("non.tab", header=TRUE, sep="\t")
+var <- read.table("var.tab", header=TRUE, sep="\t")
+
+g.c <- unlist(strsplit(unlist(reference.genome),"")) # converts to vector
+
+# ref.counts <- ref[,-c(1:2)]
+# non.counts <- non[,-c(1:2)]
+
+var.trunc <- substr(var[,4],1,1) # truncating vars to just the first nucleotide
+
+## Sequence Writer Script ##
+
+sequences <- matrix(g.c, nrow = length(g.c), ncol = length(ref[1,])) # make as many copies of the reference genome as we have samples
+
+i = 2 # pick the sample (column), this will eventually be a for loop
+
+  for (j in 1:length(ref[,1])) # cycle through all positions
+  {
+    pos <- ref[j,2] # which nucleotide does the position start on?
+    if(ref[j,i] <= non[j,i]) # does the alternative outweigh the reference?
+    {
+      sequences[pos,i] <- var.trunc[j] # change the "pos" position of the ith genome to the alt nucleotide (truncated)
+    } # else keep the reference
+  }
+
+seq <- sequences[,2]
+my_fasta_sub <- seq[names(seq)]
+write.fasta(sequences = seq, names = names(my_fasta_sub), nbchar = 80, file.out = "example.fasta")
+
+
+
