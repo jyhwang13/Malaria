28d4eae3be65713c18b4f1674d3c0795d3c1dd7f
mhukill <65314778+mhukill@users.noreply.github.com>
2020-05-14 15:28:32 -0700

    Major SNPs Update
    Two major updates:
    1) We've rejiggered the code such that it only deals with SNPs
    2) We've made a series of test trials (one for each country, some are commented out just for ease of use on ye ol' CPU, but feel free to bring them back in [lines 18-20]).
    AREAS OF CONCERN:
    1) The FASTA files are still extremely pesky...we need help converting our vectors into FASTAs
    2) Again, this just works with SNPs (that's okay for now)

--- sequence.writer.r
+++ sequence.writer.r
@@ -1,4 +1,5 @@
-# Read in the data and references ##
+## Read in the data and references ##
+## Set your working directory to Malaria if you're using GitHub Desktop ##
 library("seqinr")
 reference.genome <- read.fasta("LR605956.fasta", as.string=TRUE, seqonly=TRUE)
 ref <- read.table("ref.tab", header=TRUE, sep="\t")
@@ -7,27 +8,44 @@ var <- read.table("var.tab", header=TRUE, sep="\t")
 
 g.c <- unlist(strsplit(unlist(reference.genome),"")) # converts to vector
 
-# ref.counts <- ref[,-c(1:2)]
-# non.counts <- non[,-c(1:2)]
-
-var.trunc <- substr(var[,4],1,1) # truncating vars to just the first nucleotide
 
 ## Sequence Writer Script ##
 
 sequences <- matrix(g.c, nrow = length(g.c), ncol = length(ref[1,])) # make as many copies of the reference genome as we have samples
 
-i = 2 # pick the sample (column), this will eventually be a for loop
 
-  for (j in 1:length(ref[,1])) # cycle through all positions
+
+test.places <- c(1,87,166,363) 
+#,562,694,1685,2035,3041,4256,4348, 4413, 4862,5002, 5030, 5123,
+                 #5474, 5738, 5755, 5769, 6019,6150, 6516, 6558, 6583, 6822, 6892, 6926, 7028)
+# Mauritania, Gambia, Guinea, Gambia, Kenya, Thailand, Tanzania, Ghana, Cambodia, Indonesia,
+# Burkina Faso, Mali, Papua New Guinea, Peru, Bangladesh, Malawi, Vietnam, Colombia, Uganda, 
+# Myanmar, Laos, Congo DR, Nigeria, Madagascar, Camaroon, Ivory Coast, Ethiopia, Benin, Senegal
+# 3039-3041 has weird entries?? Errors??
+
+sequences.test <- matrix(g.c, nrow = length(g.c), ncol = length(test.places))
+
+
+## This Code ONLY does SNPs ##
+
+for(i in 1:length(test.places)) # cycle thru our test places
+{
+  for (j in 1:length(ref[,1])) # cycle through all positions on genome
   {
-    pos <- ref[j,2] # which nucleotide does the position start on?
-    if(ref[j,i] <= non[j,i]) # does the alternative outweigh the reference?
+    if((nchar(var[j,3]) == 1) && (nchar(var[j,4]) == 1)) # restricts us to just SNPs
     {
-      sequences[pos,i] <- var.trunc[j] # change the "pos" position of the ith genome to the alt nucleotide (truncated)
-    } # else keep the reference
+      pos <- ref[j,2] # which nucleotide does the position start on?
+      if(ref[j,test.places[i]+2] <= non[j,test.places[i]+2]) # does the alternative outweigh the reference?
+      {
+        sequences.test[pos,test.places[i]] <- var[j,4] # change the "pos" position of the ith genome to the alt nucleotide (truncated)
+      } # else keep the reference
+    }
   }
+}
 
-seq <- sequences[,2]
-my_fasta_sub <- seq[names(seq)]
-write.fasta(sequences = seq, names = names(my_fasta_sub), nbchar = 80, file.out = "example.fasta")
-print("hello charlotte")
+# seq <- sequences[,2]
+# my_fasta_sub <- seq[names(seq)]
+# write.fasta(sequences = seq, names = names(my_fasta_sub), nbchar = 80, file.out = "example.fasta")
+
+
+
